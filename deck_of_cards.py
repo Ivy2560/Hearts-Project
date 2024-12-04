c71611dc1d3553bf230f4ae33e4cb5f7c5704209
Ivy2560 <isabellawinter256@gmail.com>
2024-12-04 11:45:44 -0600

    deck_of_cards.py mostly finished,

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,9 +1,9 @@
 from tkinter import *
 from card_values import card_suits, card_numbers, deck_list, hearts_values, hearts_values_jack
 
-
 class PlayingCard(Canvas):
-    def __init__(self, master, number, suit, can_play=True, value=False):
+    def __init__(self, master, card, can_play=True, value=None):
+        (number, suit) = card
         if number not in card_numbers:
             raise ValueError('Invalid card number')
         if suit not in card_suits:
@@ -20,9 +20,9 @@ class PlayingCard(Canvas):
         #
         self.can_play = can_play
         #
-        if value == False:
-            if type(self.number) == int:
-                self.value = value
+        if value is None:
+            if type(self.number) == int or self.number in ['2','3','4','5','6','7','8','9','10']:
+                self.value = int(self.number)
             elif self.number == 'ace':
                 self.value = 1
             elif self.number in ['jack','queen','king']:
@@ -31,12 +31,14 @@ class PlayingCard(Canvas):
             self.value = value
 
         self.draw_card()
-        self.bind('Button-1',self.card_selected)
+        self.bind('<Button-1>',self.card_selected)
 
     def card_selected(self,event):
+        print('Got here')
         if self.can_play:
             return_value = self.value
-            self.delete()
+            self.destroy()
+            print(return_value)
             return return_value
     #
     def draw_diamond (self,x,y, color):
@@ -44,24 +46,41 @@ class PlayingCard(Canvas):
         self.create_polygon(coordinates, fill=color)
 
     def draw_heart(self,x,y,color):
-        coordinates_1 = [x-8,y-14, x,y] # bounding box is for the circle
-        self.create_arc(coordinates_1,start=0,extent=185,fill=color, outline=color)
-        coordinates_2 = [x,y-14, x+8,y]
-        self.create_arc(coordinates_2, start=-5, extent=185, fill=color, outline=color)
-        coordinates_3 = [x-8,y-9, x+9,y-9, x,y+13]
-        self.create_polygon(coordinates_3, fill=color)
+        coordinates_1 = [x,y+10, x+10,y-1, x-9,y-1]
+        self.create_polygon(coordinates_1, fill=color)
+        coordinates_2 = [x,y+12, x+8,y-2, x-8,y-2]
+        self.create_polygon(coordinates_2, fill=color)
+        #
+        coordinates_3 = [x-10,y, x,y-10]
+        self.create_oval(coordinates_3, fill=color, outline=color)
+        coordinates_4 = [x,y, x+10,y-10]
+        self.create_oval(coordinates_4, fill=color, outline=color)
 
     def draw_club(self,x, y, color):
         coordinates_1 = [x-5,y-13, x+5,y-3]
-        self.create_oval(coordinates_1, fill=color)
+        self.create_oval(coordinates_1, fill=color, outline=color)
         coordinates_2 = [x-10,y-4, x,y+6]
-        self.create_oval(coordinates_2, fill=color)
+        self.create_oval(coordinates_2, fill=color, outline=color)
         coordinates_3 = [x,y-4, x+10,y+6]
-        self.create_oval(coordinates_3, fill=color)
+        self.create_oval(coordinates_3, fill=color, outline=color)
         #
         coordinates_4 = [x,y-9, x+4,y+1, x-4,y+1]
         self.create_polygon(coordinates_4, fill=color)
         coordinates_5 = [x,y, x+4,y+12, x-4,y+12]
+        self.create_polygon(coordinates_5, fill=color)
+
+    def draw_spade(self, x, y, color):
+        coordinates_1 = [x,y-9, x+9,y-2, x-9,y-2]
+        self.create_polygon(coordinates_1, fill=color)
+        coordinates_2 = [x,y-12, x+7,y, x-7,y]
+        self.create_polygon(coordinates_2, fill=color)
+        #
+        coordinates_3 = [x-10,y-3, x,y+7]
+        self.create_oval(coordinates_3, fill=color, outline=color)
+        coordinates_4 = [x,y-3, x+10,y+7]
+        self.create_oval(coordinates_4, fill=color, outline=color)
+        #
+        coordinates_5 = [x,y+1, x+4,y+11, x-4,y+11]
         self.create_polygon(coordinates_5, fill=color)
 
     def draw_char(self,x, y , char, color):
@@ -69,30 +88,43 @@ class PlayingCard(Canvas):
 
     #
     def draw_card(self):
-        pass
+        if self.suit == 'club':
+            color = 'black'
+            self.draw_club(20, 55, color)
+        elif self.suit == 'heart':
+            color = 'red'
+            self.draw_heart(20, 55, color)
+        elif self.suit == 'spade':
+            color = 'black'
+            self.draw_spade(20, 55, color)
+        else: # self.suit == 'diamond':
+            color = 'red'
+            self.draw_diamond(20, 55, color)
+
+        if type(self.number) == str: # A, J, Q, K
+            char = self.number[0].upper()
+        else:
+            char = str(self.number)
+        self.draw_char(20,25,char, color)
 
 
     def __str__(self):
         return f'{self.number} of {self.suit}s'
 
 
-class DeckOfCards():
-    def __init__(self,gameValues):
-        pass
-
-
 window = Tk()
 window.title('Hearts')
 window.geometry('1000x700')
-my_card = PlayingCard(window,'ace','spade')
-my_card.grid(row=0,column=0)
 
-my_card.draw_char(20,25,'K', 'black')
-#my_card.draw_diamond(19,57, 'red')
-my_card.draw_club(20,55, 'black')
-#my_card.draw_heart(80,30)
+def show_cards(start_card):
+    for c in range(6):
+        for r in range(3):
+            i = c+1 + 6*r - 1 + start_card-1
+            if i >= 52:
+                break
+            my_card = PlayingCard(window,(deck_list[i]))
+            my_card.grid(row=r,column=c)
 
-
-
+show_cards(1)
 
 window.mainloop()
